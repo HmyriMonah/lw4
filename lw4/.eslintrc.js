a87d218e5fbabf1ea061719f6d88303669a26881
HmyriMonah <sankoptelin@gmail.com>
2020-12-20 23:12:35 +0400

    version 1

---
+++ lw4/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "es2020": true,
+        "browser":true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+      "linebreak-style":["error","windows"]
+    }
+};
