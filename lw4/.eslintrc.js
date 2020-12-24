7eefb178c7c55ba0831068c0712ee6c48b05ca58
HmyriMonah <sankoptelin@gmail.com>
2020-12-24 12:01:33 +0400

    Create

--- lw4/.eslintrc.js
+++ lw4/.eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
-    "env": {
-        "es2020": true,
-        "browser":true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-      "linebreak-style":["error","windows"]
-    }
-};
+  "env": {
+    "es2020": true,
+    "browser":true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 12
+  },
+  "rules": {
+    "linebreak-style":["error","windows"]
+  },
+  "parserOptions": {
+    "sourceType": "module",
+  }
+  };
